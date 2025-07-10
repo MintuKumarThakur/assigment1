ddf5524749f5f7c09adf022bbd2d3fabfaca197d
MintuKumarThakur <123482791+MintuKumarThakur@users.noreply.github.com>
2025-07-10 21:05:39 +0530

    Add files via upload

---
+++ ASSIGMENT 1/task 1.py
@@ -0,0 +1,13 @@
+# task 1 
+
+a=float(input('Enter the first number:'))
+b=float(input('Enter the secound number:'))
+
+c= float(a+b)
+print('addition', c)
+c= float(a-b)
+print('subraction', c)
+c= float(a*b)
+print('multiplication', c)
+c= float(a/b)
+print('division', c)
---
+++ ASSIGMENT 1/task 2.py
@@ -0,0 +1,6 @@
+# task 2
+
+b=input('enter your first name:')
+c=input('enter your last name:')
+e=b+c
+print("hello,", e + "! welcome to python program.")
